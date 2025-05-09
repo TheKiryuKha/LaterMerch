4ef53f7f66b0b3614574a39d6caf57ae4c75025a
TheKiryuKha <Kirillzuyeu@gmail.com>
2025-05-09 21:38:17 +0300

    chore: adds Product entity

---
+++ tests/Unit/Models/ProductTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\Product;
+
+test('to array', function () {
+    $product = Product::factory()->create()->fresh();
+
+    expect(array_keys($product->toArray()))->toBe([
+        'id',
+        'title',
+        'price',
+        'description',
+        'created_at',
+        'updated_at',
+    ]);
+});
