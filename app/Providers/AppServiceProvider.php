5bc43dca7f3c3978756a7e06eef3b8cf875b41f0
TheKiryuKha <Kirillzuyeu@gmail.com>
2025-05-09 19:26:14 +0300

    chore: configure some defaults

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use DB;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
@@ -21,6 +24,24 @@ final class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configureUrls();
+    }
+
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(app()->isProduction());
+    }
+
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict();
+        Model::unguard();
+    }
+
+    private function configureUrls(): void
+    {
+        URL::forceHttps(app()->isProduction());
     }
 }
